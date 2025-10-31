179861ee900a29478d9c76a5d8253fa78ba769c5
Andy Ansong <andyansong82@gmail.com>
2025-10-31 18:54:01 +0000

    init redis

--- Program.cs
+++ Program.cs
@@ -25,27 +25,6 @@ builder.Services.AddDbContext<BookStoreContext>(options =>
 );
 builder.Services.AddScoped<IBookService, BookService>();
 
-var jwtSettings = builder.Configuration.GetSection("Jwt");
-var key = Encoding.ASCII.GetBytes(jwtSettings["Key"]);
-
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-})
-.AddJwtBearer(options =>
-{
-    options.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuer = true,
-        ValidateAudience = true,
-        ValidateLifetime = true,
-        ValidateIssuerSigningKey = true,
-        ValidIssuer = jwtSettings["Issuer"],
-        ValidAudience = jwtSettings["Audience"],
-        IssuerSigningKey = new SymmetricSecurityKey(key)
-    };
-});
 
 builder.Services.AddAuthorization();
 
