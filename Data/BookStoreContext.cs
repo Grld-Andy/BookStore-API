1170dd55b26848ae0a99963f659bb9e5341e2d25
Andy Ansong <andyansong82@gmail.com>
2025-11-07 23:08:39 +0000

    create user table

--- Data/BookStoreContext.cs
+++ Data/BookStoreContext.cs
@@ -7,6 +7,7 @@ public class BookStoreContext : DbContext
     public BookStoreContext(DbContextOptions<BookStoreContext> options) : base(options) { }
     
     public DbSet<Book> Books { get; set; }
+    public DbSet<User> Users { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
--- Models/User.cs
+++ Models/User.cs
@@ -1,7 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace BookStore.Models;
 
+[Table("Users")]
 public class User
 {
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+
+    [Column("username")]
+    [Required]
     public string Username { get; set; } = string.Empty;
+
+    [Column("passwordHash")]
+    [Required]
     public string PasswordHash { get; set; } = string.Empty;
 }
