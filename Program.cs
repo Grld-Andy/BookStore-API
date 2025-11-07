7992afc8edf3e3b851bd342cf3397e7b4d253c6f
Andy Ansong <andyansong82@gmail.com>
2025-11-07 21:07:56 +0000

    setup dependencies in config

--- Program.cs
+++ Program.cs
@@ -1,40 +1,12 @@
 global using BookStore.Models;
 global using BookStore.Services;
-global using BookStore.Profiles;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
-using BookStore.Data;
-using Microsoft.EntityFrameworkCore;
 using BookStore.Middlewares;
+using BookStore.Startup;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddSingleton<MongoDbService>();
-builder.Services.AddAutoMapper(typeof(Program).Assembly);
-
-builder.Services.AddDbContext<BookStoreContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("MsSqlDbConnection"))
-);
-builder.Services.AddStackExchangeRedisCache(option =>
-{
-    option.Configuration = builder.Configuration.GetConnectionString("Redis");
-    option.InstanceName = "Books_";
-});
-
-builder.Services.AddScoped<IRedisCacheService, RedisCacheService>();
-builder.Services.AddScoped<IBookService, BookService>();
-builder.Services.AddTransient<GlobalErrorHandler>();
-
-builder.Services.AddAuthorization();
+builder.AddDependencies();
 
 var app = builder.Build();
 
---
+++ Startup/DependenciesConfig.cs
@@ -0,0 +1,35 @@
+using BookStore.Data;
+using BookStore.Middlewares;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.Startup;
+
+public static class DependenciesConfig
+{
+    public static void AddDependencies(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddControllers();
+
+        // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+        builder.Services.AddOpenApi();
+
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        builder.Services.AddAutoMapper(typeof(Program).Assembly);
+
+        builder.Services.AddDbContext<BookStoreContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("MsSqlDbConnection"))
+        );
+        builder.Services.AddStackExchangeRedisCache(option =>
+        {
+            option.Configuration = builder.Configuration.GetConnectionString("Redis");
+            option.InstanceName = "Books_";
+        });
+
+        builder.Services.AddScoped<IRedisCacheService, RedisCacheService>();
+        builder.Services.AddScoped<IBookService, BookService>();
+        builder.Services.AddTransient<GlobalErrorHandler>();
+
+        builder.Services.AddAuthorization();
+    }
+}
